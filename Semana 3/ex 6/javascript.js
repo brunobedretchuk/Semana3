36cda63bae5953fa115f8d986ac9f76ccc86c73f
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-03-17 20:15:15 -0300

    Ex 6 - Terminado

--- Semana 3/ex 6/javascript.js
+++ Semana 3/ex 6/javascript.js
@@ -1,8 +1,45 @@
-var relogio = document.querySelector('p');
-function time() {
-    var date = new Date();
-    var horario = [date.getHours(), date.getMinutes(), date.getSeconds()];
-    relogio.innerHTML = `${horario[0]}h:${horario[1]}m:${horario[2]}s`;   
+var input = document.querySelector('input');
+var button = document.querySelector('button');
+var date = new Date();
+var dataHoje = {ano :date.getFullYear(), mes :date.getMonth() + 1, dia :date.getDate()};
+var dataNasc = {};
+var idade;
+var output = document.querySelector('p');
+
+
+// function getDataNasc() {
+//     if (!input.value) {
+//         console.log('error, no date chosen!')
+//     }
+//     else {
+//         dataNasc.ano = parseInt(input.value.substring(0, 4));
+//         dataNasc.mes = parseInt(input.value.substring(5, 7));
+//         dataNasc.dia = parseInt(input.value.substring(8, 10));
+//     }
+// }
+
+function calcAge() {
+
+    if (!input.value) {
+        console.log('error, no date chosen!')
+    }
+    else {
+        dataNasc.ano = parseInt(input.value.substring(0, 4));
+        dataNasc.mes = parseInt(input.value.substring(5, 7));
+        dataNasc.dia = parseInt(input.value.substring(8, 10));
+    }
+
+
+    if (dataHoje.mes > dataNasc.mes || dataHoje.mes === dataNasc.mes && dataHoje.dia >= dataNasc.dia){
+idade = dataHoje.ano - dataNasc.ano;
+console.log(idade);
+}
+else{
+    idade = dataHoje.ano - dataNasc.ano - 1;
+    console.log(idade);
 }
 
-const relogitows = setInterval(time , 1000);
+output.innerHTML = `Você tem <b>${idade}</b> anos de idade!`
+}
+
+button.addEventListener('click' , calcAge);
